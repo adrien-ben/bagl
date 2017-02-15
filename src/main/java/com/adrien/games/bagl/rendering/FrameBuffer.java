9d85f162641fa45dec91e7135e7b61724b01fb64
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-02-15 17:28:14 +0100

    FrameBuffer support MRT

--- src/main/java/com/adrien/games/bagl/rendering/FrameBuffer.java
+++ src/main/java/com/adrien/games/bagl/rendering/FrameBuffer.java
@@ -1,14 +1,15 @@
 package com.adrien.games.bagl.rendering;
 
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.glDrawBuffers;
 import static org.lwjgl.opengl.GL30.*;
 
 import com.adrien.games.bagl.core.Color;
 
 /**
  * This class creates an OpenGL {@link FrameBuffer} object. (version >= 3.0).
- * <p>It binds a render buffer to if for the depth component and a texture (that you can retrieve 
- * for later use) as the color buffer. 
+ * <p>It binds a render buffer to if for the depth component and at least one texture (that you can 
+ * retrieve for later use) as the color buffers. 
  * <br>When the frame buffer is bound OpenGL will render into this frame buffer. If no frame buffer
  * is bound OpenGL will then render in the default frame buffer.
  * 
@@ -20,8 +21,8 @@ public class FrameBuffer {
 	private final int handle;
 	private final int depthBufferHandle;
 	private final int width;
-	private final int height;
-	private final Texture colorTexture;
+	private final int height; 
+	private final Texture[] colorOutputs;
 	
 	/**
 	 * Creates a new {@link FrameBuffer}.
@@ -29,27 +30,68 @@ public class FrameBuffer {
 	 * @param height The height of the frame buffer.
 	 */
 	public FrameBuffer(int width, int height) {
+		this(width, height, 1);
+	}
+	
+	/**
+	 * Creates a new {@link FrameBuffer}.
+	 * @param width The width of the frame buffer.
+	 * @param height The height of the frame buffer.
+	 * @param colorOutputs The number of color outputs.
+	 */
+	public FrameBuffer(int width, int height, int colorOutputs) {
 		this.width = width;
 		this.height = height;
 		this.depthBufferHandle = this.createDepthBuffer(this.width, this.height);
-		this.colorTexture = new Texture(this.width, this.height);
-		this.handle = this.createBuffer(this.colorTexture.getHandle(), this.depthBufferHandle);
+		this.colorOutputs = this.createColorOutputs(colorOutputs, this.width, this.height);
+		this.handle = this.createBuffer(this.colorOutputs, this.depthBufferHandle);
+	}
+	
+	/**
+	 * Creates all textures to use as the different color outputs.
+	 * @param colorOutputs The number of texture to generate.
+	 * @param width The width of the frame buffer.
+	 * @param height The height of the frame buffer.
+	 * @return An array of {@link Texture}.
+	 */
+	private Texture[] createColorOutputs(int colorOutputs, int width, int height) {
+		Texture[] textures = new Texture[colorOutputs];
+		for(int i = 0; i < colorOutputs; i++) {
+			textures[i] = new Texture(width, height);
+		}
+		return textures;
 	}
 
 	/**
 	 * Create the actual OpenGL frame buffer object and attaches
-	 * the color buffer texture and the depth render buffer.
-	 * @param textureHandle The handle of the OpenGL texture.
+	 * the color buffer textures and the depth render buffer.
+	 * @param textures The textures to use as color outputs.
 	 * @param depthBufferHandle The handle of the OpenGL render buffer.
 	 * @return The handle of the OpengGL frame buffer.
 	 */
-	private int createBuffer(int textureHandle, int depthBufferHandle) {
+	private int createBuffer(Texture[] textures, int depthBufferHandle) {
 		int bufferHandle = glGenFramebuffers();
 		glBindFramebuffer(GL_FRAMEBUFFER, bufferHandle);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureHandle, 0);
+		for(int i = 0; i < textures.length; i++) {			
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, textures[i].getHandle(), 0);
+		}
 		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBufferHandle);
+		glDrawBuffers(this.generateBuffersToDraw(textures.length));
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		return bufferHandle;
+	}
+	
+	/**
+	 * Generates an array containing the buffers to draw.
+	 * @param count The number of buffer to draw.
+	 * @return An array of integer.
+	 */
+	private int[] generateBuffersToDraw(int count) {
+		int[] buffers = new int[count];
+		for(int i = 0; i < count; i++) {
+			buffers[i] = GL_COLOR_ATTACHMENT0 + i;
+		}
+		return buffers;
 	}
 	
 	/**
@@ -101,7 +143,9 @@ public class FrameBuffer {
 	 * Releases resources.
 	 */
 	public void destroy() {
-		this.colorTexture.destroy();
+		for(Texture t : colorOutputs) {
+			t.destroy();
+		}
 		glDeleteRenderbuffers(this.depthBufferHandle);
 		glDeleteFramebuffers(this.handle);
 	}
@@ -119,7 +163,11 @@ public class FrameBuffer {
 	}
 
 	public Texture getColorTexture() {
-		return colorTexture;
+		return this.getColorTexture(1);
+	}
+	
+	public Texture getColorTexture(int index) {
+		return colorOutputs[index];
 	}
 	
 }
