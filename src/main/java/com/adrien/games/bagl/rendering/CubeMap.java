260ecb61d748240f6ae8870544bc1e880c2e5a07
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-02-17 16:51:55 +0100

    Created CubeMap class

---
+++ src/main/java/com/adrien/games/bagl/rendering/CubeMap.java
@@ -0,0 +1,84 @@
+package com.adrien.games.bagl.rendering;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.*;
+import static org.lwjgl.opengl.GL13.*;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.stb.STBImage;
+
+/**
+ * 
+ * OpenGL cube map.
+ *
+ */
+public class CubeMap {
+
+	private int handle;
+	
+	/**
+	 * Creates a new CubeMap from the different faces file path. 
+	 * @param left The path to the file containing the left face.
+	 * @param right The path to the file containing the right face.
+	 * @param bottom The path to the file containing the bottom face.
+	 * @param top The path to the file containing the top face.
+	 * @param back The path to the file containing the back face.
+	 * @param front The path to the file containing the front face.
+	 */
+	public CubeMap(String left, String right, String bottom, String top, String back, String front) {
+		this.handle = glGenTextures();
+		glBindTexture(GL_TEXTURE_CUBE_MAP, this.handle);
+        
+        this.loadImageFromPath(left, GL_TEXTURE_CUBE_MAP_NEGATIVE_X);
+        this.loadImageFromPath(right, GL_TEXTURE_CUBE_MAP_POSITIVE_X);
+        this.loadImageFromPath(bottom, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y);
+        this.loadImageFromPath(top, GL_TEXTURE_CUBE_MAP_POSITIVE_Y);
+        this.loadImageFromPath(back, GL_TEXTURE_CUBE_MAP_POSITIVE_Z);
+        this.loadImageFromPath(front, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z);
+        
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+	}
+	
+	private void loadImageFromPath(String path, int target) {
+		IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer comp = BufferUtils.createIntBuffer(1);
+		STBImage.stbi_set_flip_vertically_on_load(0);
+		ByteBuffer image = STBImage.stbi_load(path, width, height, comp, 3);
+		if(image == null) {
+			throw new RuntimeException("Failed to load a face from the cubemap : '" + path + "'.");
+		}
+		glTexImage2D(target, 0, GL_RGB8, width.get(), height.get(), 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	}
+	
+	/**
+	 * Binds the cube map.
+	 */
+	public void bind() {
+		glBindTexture(GL_TEXTURE_CUBE_MAP, this.handle);
+	}
+	
+	/**
+	 * Unbinds the cube map.
+	 */
+	public static void unbind() {
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+	}
+	
+	/**
+	 * Release resources.
+	 */
+	public void destroy() {
+		glDeleteTextures(this.handle);
+	}
+	
+}
--- src/main/java/com/adrien/games/bagl/sample/SkyboxSample.java
+++ src/main/java/com/adrien/games/bagl/sample/SkyboxSample.java
@@ -1,17 +1,8 @@
 package com.adrien.games.bagl.sample;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL12.*;
-import static org.lwjgl.opengl.GL13.*;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.lwjgl.BufferUtils;
 
 import com.adrien.games.bagl.core.Camera;
 import com.adrien.games.bagl.core.Engine;
@@ -19,15 +10,12 @@ import com.adrien.games.bagl.core.Game;
 import com.adrien.games.bagl.core.Matrix4;
 import com.adrien.games.bagl.core.Time;
 import com.adrien.games.bagl.core.Vector3;
+import com.adrien.games.bagl.rendering.CubeMap;
 import com.adrien.games.bagl.rendering.IndexBuffer;
 import com.adrien.games.bagl.rendering.Shader;
 import com.adrien.games.bagl.rendering.Vertex;
 import com.adrien.games.bagl.rendering.VertexBuffer;
 import com.adrien.games.bagl.rendering.VertexPosition;
-import com.adrien.games.bagl.utils.MeshFactory;
-
-import de.matthiasmann.twl.utils.PNGDecoder;
-import de.matthiasmann.twl.utils.PNGDecoder.Format;
 
 public class SkyboxSample {
 
@@ -42,7 +30,7 @@ public class SkyboxSample {
 		
 		private Shader shader;
 		private Camera camera;
-		private int cubemap;
+		private CubeMap cubemap;
 		
 		@Override
 		public void init() {
@@ -54,35 +42,22 @@ public class SkyboxSample {
 			this.shader.addFragmentShader("skybox.frag");
 			this.shader.compile();
 			
-			this.camera = new Camera(Vector3.ZERO, new Vector3(1, -0.5f, 1), Vector3.UP, (float)Math.toRadians(60), 
+			this.camera = new Camera(Vector3.ZERO, new Vector3(0, -0.5f, -1), Vector3.UP, (float)Math.toRadians(60), 
 					(float)WIDTH/HEIGHT, 1, 100);
 			
-			this.cubemap = glGenTextures();
-			glBindTexture(GL_TEXTURE_CUBE_MAP, this.cubemap);
-			
-			this.loadImageBuffer(new File(MeshFactory.class.getResource("/skybox/left.png").getFile())
-					.getAbsolutePath(), GL_TEXTURE_CUBE_MAP_NEGATIVE_X);
-			this.loadImageBuffer(new File(MeshFactory.class.getResource("/skybox/right.png").getFile())
-					.getAbsolutePath(), GL_TEXTURE_CUBE_MAP_POSITIVE_X);
-			this.loadImageBuffer(new File(MeshFactory.class.getResource("/skybox/bottom.png").getFile())
-					.getAbsolutePath(), GL_TEXTURE_CUBE_MAP_NEGATIVE_Y);
-			this.loadImageBuffer(new File(MeshFactory.class.getResource("/skybox/top.png").getFile())
-					.getAbsolutePath(), GL_TEXTURE_CUBE_MAP_POSITIVE_Y);
-			this.loadImageBuffer(new File(MeshFactory.class.getResource("/skybox/back.png").getFile())
-					.getAbsolutePath(), GL_TEXTURE_CUBE_MAP_NEGATIVE_Z);
-			this.loadImageBuffer(new File(MeshFactory.class.getResource("/skybox/front.png").getFile())
-					.getAbsolutePath(), GL_TEXTURE_CUBE_MAP_POSITIVE_Z);
-			
-			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-			
-			glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+			this.cubemap = new CubeMap(this.getResourcePath("/skybox/left.png"),
+					this.getResourcePath("/skybox/right.png"),
+					this.getResourcePath("/skybox/bottom.png"),
+					this.getResourcePath("/skybox/top.png"),
+					this.getResourcePath("/skybox/back.png"),
+					this.getResourcePath("/skybox/front.png"));
 			
 			glEnable(GL_DEPTH_TEST);
 			glEnable(GL_CULL_FACE);
+		}
+		
+		private String getResourcePath(String resource) {
+			return new File(TestGame.class.getResource(resource).getFile()).getAbsolutePath();
 		}
 		
 		private void initMesh() {
@@ -110,20 +85,6 @@ public class SkyboxSample {
 			this.indexBuffer = new IndexBuffer(indices);
 		}
 		
-		private void loadImageBuffer(String path, int target) {
-			try (InputStream in = Files.newInputStream(Paths.get(path))) {
-				PNGDecoder decoder = new PNGDecoder(in);
-				int width = decoder.getWidth();
-				int height = decoder.getHeight();
-				ByteBuffer byteBuffer = BufferUtils.createByteBuffer(width*height*3);
-				decoder.decodeFlipped(byteBuffer, width*3, Format.RGB);
-				byteBuffer.flip();
-				glTexImage2D(target, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, byteBuffer);
-			} catch (IOException e) {
-				throw new RuntimeException("Failed to load texture '" + path + "'.", e);
-			}
-		}
-		
 		@Override
 		public void update(Time time) {
 		}
@@ -133,7 +94,7 @@ public class SkyboxSample {
 			
 			this.vertexBuffer.bind();
 			this.indexBuffer.bind();
-			glBindTexture(GL_TEXTURE_CUBE_MAP, this.cubemap);
+			this.cubemap.bind();
 			this.shader.bind();
 			Matrix4.mul(this.camera.getViewProj(), Matrix4.createRotation(Vector3.UP, 
 					(float)Math.toRadians(0.05)), this.camera.getViewProj());
@@ -142,7 +103,7 @@ public class SkyboxSample {
 			glDrawElements(GL_TRIANGLES, this.indexBuffer.getSize(), GL_UNSIGNED_INT, 0);
 			
 			Shader.unbind();
-			glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+			CubeMap.unbind();
 			IndexBuffer.unbind();
 			VertexBuffer.unbind();
 		}
