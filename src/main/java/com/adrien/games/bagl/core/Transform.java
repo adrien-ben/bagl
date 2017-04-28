49c90f68df8b1e02832df40ab4f700196b80df10
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-04-28 11:41:36 +0200

    Refactored

--- src/main/java/com/adrien/games/bagl/core/Transform.java
+++ src/main/java/com/adrien/games/bagl/core/Transform.java
@@ -32,14 +32,29 @@ public class Transform {
 		this.scaleBuff = Matrix4.createZero();
 	}
 	
-	public void transform(Transform transform) {		
+	/**
+	 * Transforms this transform relatively to another transform.
+	 * This transform is 'sent' to the space of the other transform.
+	 * @param transform The transform to apply.
+	 * @return This for chaining.
+	 */
+	public Transform transform(Transform transform) {		
 		final Matrix4 tm = transform.getTransformMatrix();
 		this.translation.transform(tm, 1);
 		this.scale.transform(tm, 0);
 		this.rotation.mul(transform.getRotation());
 		this.isDirty = true;
+		return this;
 	}
 	
+	/**
+	 * Transforms a transform relatively to another transform.
+	 * The transform <code>toTransform</code> is 'sent' to the 
+	 * space of the other transform.
+	 * @param toTransform The transform to apply a transform to. Will not be changed.
+	 * @param transform The transform to apply. Will not be changed.
+	 * @param result The transform where to store the result.
+	 */
 	public static void transform(Transform toTransform, Transform transform, Transform result) {
 		final Matrix4 tm = transform.getTransformMatrix();
 		Vector3.transform(tm, toTransform.translation, 1, result.translation);
@@ -48,16 +63,27 @@ public class Transform {
 		result.isDirty = true;
 	}
 	
+	private void computeTransform() {
+		this.transBuff.setTranslation(this.translation);
+		this.rotBuff.setRotation(this.rotation);
+		this.scaleBuff.setScale(this.scale);
+		
+		Matrix4.mul(this.transBuff, this.rotBuff, this.transform);
+		Matrix4.mul(this.transform, this.scaleBuff, this.transform);
+		
+		this.isDirty = false;
+	}
+	
+	/**
+	 * Returns the matrix of the transform.
+	 * <p>This matrix contains the combination of translation,
+	 * rotation and scaling. The matrix is computed here if 
+	 * needed.  
+	 * @return A matrix.
+	 */
 	public Matrix4 getTransformMatrix() {
 		if(this.isDirty) {
-			this.transBuff.setTranslation(this.translation);
-			this.rotBuff.setRotation(this.rotation);
-			this.scaleBuff.setScale(this.scale);
-			
-			Matrix4.mul(this.transBuff, this.rotBuff, this.transform);
-			Matrix4.mul(this.transform, this.scaleBuff, this.transform);
-			
-			this.isDirty = false;
+			this.computeTransform();
 		}
 		return this.transform;
 	}
@@ -74,19 +100,40 @@ public class Transform {
 		return scale;
 	}
 
-	public void setTranslation(Vector3 translation) {
+	/**
+	 * Sets the translation. Flags the transform as dirty
+	 * so the transform matrix is recomputed when needed. 
+	 * @param translation The translation vector to set.
+	 * @return This for chaining.
+	 */
+	public Transform setTranslation(Vector3 translation) {
 		this.translation = translation;
 		this.isDirty = true;
+		return this;
 	}
 
-	public void setRotation(Quaternion rotation) {
+	/**
+	 * Sets the rotation. Flags the transform as dirty
+	 * so the transform matrix is recomputed when needed. 
+	 * @param rotation The rotation to set.
+	 * @return This for chaining.
+	 */
+	public Transform setRotation(Quaternion rotation) {
 		this.rotation = rotation;
 		this.isDirty = true;
+		return this;
 	}
 
-	public void setScale(Vector3 scale) {
+	/**
+	 * Sets the scaling. Flags the transform as dirty
+	 * so the transform matrix is recomputed when needed. 
+	 * @param scale The scale vector to set.
+	 * @return This for chaining.
+	 */
+	public Transform setScale(Vector3 scale) {
 		this.scale = scale;
 		this.isDirty = true;
+		return this;
 	}
 	
 }
