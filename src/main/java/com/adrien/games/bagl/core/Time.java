a4bd3b917d97f803d19246f2aa31948dbde03fef
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-04-28 07:20:14 +0200

    Fixed Time class
    Timings are now internally store and computed as nanosecond (as longs)
    for more precision. A weird bug appeared on some machines causing
    animations to appear laggy.

--- src/main/java/com/adrien/games/bagl/core/Time.java
+++ src/main/java/com/adrien/games/bagl/core/Time.java
@@ -1,36 +1,52 @@
 package com.adrien.games.bagl.core;
 
+/**
+ * Simple time class allowing retrieval of elapsed time since 
+ * last frame and total program time.
+ *
+ */
 public final class Time {
 	
 	private static final long NS_PER_SEC = 1000000000L;
 	
-	private float elapsed;
-	private float total;
-	private float time;
+	private long elapsed;
+	private long total;
+	private long time;
 	
 	public Time() {
 		this.elapsed = 0;
 		this.total = 0;
 		this.time = time();
 	}
-	
+
+	/**
+	 * Updates the timings
+	 */
 	public void update() {
-		float newTime = time();
+		long newTime = time();
 		elapsed = newTime - time;
 		total += elapsed;
 		time = newTime;
 	}
 	
-	private float time() {
-		return (float)System.nanoTime()/NS_PER_SEC;
+	private long time() {
+		return System.nanoTime();
 	}
 	
+	/**
+	 * Returns the time of the last frame in seconds.
+	 * @return The time of the last frame.
+	 */
 	public float getElapsedTime() {
-		return elapsed;
+		return (float)elapsed/NS_PER_SEC;
 	}
 	
+	/**
+	 * Returns the total time of the program in seconds.
+	 * @return The total time of the program.
+	 */
 	public float getTotalTime() {
-		return total;
+		return (float)total/NS_PER_SEC;
 	}
 	
 }
