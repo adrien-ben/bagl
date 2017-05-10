aa50097ed941f0ab80c763f0641023f261a84fda
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-05-10 06:42:14 +0200

    feat (particles) : added particles simulation
    this is basic stuff though

---
+++ src/main/java/com/adrien/games/bagl/rendering/particles/Particle.java
@@ -0,0 +1,81 @@
+package com.adrien.games.bagl.rendering.particles;
+
+import com.adrien.games.bagl.core.Color;
+import com.adrien.games.bagl.core.Time;
+import com.adrien.games.bagl.core.math.Vector3;
+
+public class Particle {
+
+	private Vector3 position;
+	private Vector3 direction;
+	private float speed;
+	private Color color;
+	private float ttl;
+	
+	private boolean alive;
+	
+	public Particle(Vector3 position, Vector3 direction, float speed, Color color, float ttl) {
+		this.reset(position, direction, speed, color, ttl);
+	}
+	
+	public Particle() {
+		this(new Vector3(), new Vector3(), 0, Color.WHITE, 0);
+	}
+	
+	public void update(Time time) {
+		this.alive = this.ttl > 0;
+		if(this.alive) {
+			final float elapsedTime = time.getElapsedTime();
+			this.ttl -= elapsedTime;
+			this.direction.normalise();
+			this.direction.scale(elapsedTime*this.speed);
+			this.position.add(this.direction);
+		}
+	}
+	
+	public void reset(Vector3 position, Vector3 direction, float speed, Color color, float ttl) {
+		this.position = position;
+		this.direction = direction;
+		this.speed = speed;
+		this.color = color;
+		this.ttl = ttl;
+		this.alive = this.ttl > 0;
+	}
+	
+	public boolean isAlive() {
+		return alive;
+	}
+	
+	public Vector3 getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector3 position) {
+		this.position = position;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Vector3 getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Vector3 direction) {
+		this.direction = direction;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+}
---
+++ src/main/java/com/adrien/games/bagl/rendering/particles/ParticleEmitter.java
@@ -0,0 +1,66 @@
+package com.adrien.games.bagl.rendering.particles;
+
+import java.util.function.Consumer;
+
+import com.adrien.games.bagl.core.Time;
+
+public class ParticleEmitter {
+
+	public static final int MAX_PARTICLE_COUNT = 100000;
+	
+	private float rate;
+	private int batchSize;
+	
+	private final Particle[] pool;
+	private float timeToNextBatch;
+	private Consumer<Particle> initializer;
+	
+	public ParticleEmitter(float rate, int batchSize, Consumer<Particle> initializer) {
+		this.rate = rate;
+		this.batchSize = batchSize;
+		this.pool = initPool();
+		this.timeToNextBatch = rate;
+		this.initializer = initializer;
+	}
+	
+	private static Particle[] initPool() {
+		final Particle[] pool = new Particle[MAX_PARTICLE_COUNT];
+		for(int i = 0; i < MAX_PARTICLE_COUNT; i++) {
+			pool[i] = new Particle();
+		}
+		return pool; 
+	}
+	
+	public void update(Time time) {
+		for(Particle p : this.pool) {
+			p.update(time);
+		}
+		
+		this.timeToNextBatch -= time.getElapsedTime();
+		if(this.timeToNextBatch <= 0) {
+			this.generateBatch();
+		}
+		
+	}
+	
+	private void generateBatch() {
+		this.timeToNextBatch = this.rate;
+		int generated = 0;
+		for(int i = 0; i < MAX_PARTICLE_COUNT; i++) {
+			if(generated == this.batchSize) {
+				break;
+			}
+			
+			final Particle p = this.pool[i];
+			if(!p.isAlive()) {
+				this.initializer.accept(p);
+				generated++;
+			}
+		}
+	}
+	
+	public Particle[] getParticles() {
+		return this.pool;
+	}
+	
+}
