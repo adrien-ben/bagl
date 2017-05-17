91ef89e363d20e773565d8ba810c62199f8b1104
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-05-17 07:22:53 +0200

    optim (text) : optimized text rendering
    Use buffer instead of recreating the buffers at each draw

--- src/main/java/com/adrien/games/bagl/rendering/text/TextRenderer.java
+++ src/main/java/com/adrien/games/bagl/rendering/text/TextRenderer.java
@@ -21,16 +21,52 @@ public class TextRenderer {
 
     private static final String TEXT_VERTEX_SHADER = "text.vert";
     private static final String TEXT_FRAGMENT_SHADER = "text.frag";
+    private static final int MAX_TEXT_LENGTH = 1000;
     private static final int VERTEX_PER_CHAR = 4;
     private static final int INDEX_PER_CHAR = 6;
     private static final float HALF_SCREEN_SIZE = 1f;
+
+    private TextVertex[] buffer;
+    private VertexBuffer vertexBuffer;
+    private IndexBuffer indexBuffer;
 
     private Configuration configuration;
     private Shader shader;
 
     public TextRenderer() {
+        this.indexBuffer = initIndexBuffer();
+        this.vertexBuffer = initVertexBuffer();
+        this.buffer = initVertices();
+
         this.configuration = Configuration.getInstance();
         this.shader = new Shader().addVertexShader(TEXT_VERTEX_SHADER).addFragmentShader(TEXT_FRAGMENT_SHADER).compile();
+    }
+
+    private static TextVertex[] initVertices() {
+        final TextVertex[] vertices = new TextVertex[MAX_TEXT_LENGTH*VERTEX_PER_CHAR];
+        for(int i = 0; i < MAX_TEXT_LENGTH*VERTEX_PER_CHAR; i++) {
+            vertices[i] = new TextVertex(new Vector2(), new Vector2(), new Color(1, 1, 1));
+        }
+        return vertices;
+    }
+
+    private static VertexBuffer initVertexBuffer() {
+        return new VertexBuffer(TextVertex.VERTEX_DESCRIPTION, BufferUsage.DYNAMIC_DRAW,
+                MAX_TEXT_LENGTH*VERTEX_PER_CHAR);
+    }
+
+    private static IndexBuffer initIndexBuffer() {
+        final int[] indices = new int[MAX_TEXT_LENGTH*INDEX_PER_CHAR];
+        for(int i = 0 ; i < MAX_TEXT_LENGTH; i++) {
+            int indicesIndex = i*INDEX_PER_CHAR;
+            indices[indicesIndex++] = i*VERTEX_PER_CHAR;
+            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 1;
+            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 2;
+            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 2;
+            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 1;
+            indices[indicesIndex] = i*VERTEX_PER_CHAR + 3;
+        }
+        return new IndexBuffer(BufferUsage.DYNAMIC_DRAW, indices);
     }
 
     /**
@@ -38,6 +74,8 @@ public class TextRenderer {
      */
     public void destroy() {
         this.shader.destroy();
+        this.vertexBuffer.destroy();
+        this.indexBuffer.destroy();
     }
 
     /**
@@ -64,10 +102,7 @@ public class TextRenderer {
 
         int charCount = 0;
         final int textLength = text.length();
-        final TextVertex[] vertices = new TextVertex[textLength*VERTEX_PER_CHAR];
         for(int i = 0 ; i < textLength; i++) {
-
-            int verticesIndex = charCount*VERTEX_PER_CHAR;
 
             final char c = text.charAt(i);
             if(c == '\n' || c == '\r') {
@@ -82,14 +117,34 @@ public class TextRenderer {
                     final float bottom = caret.getY() + glyph.getYOffset()*vScale;
                     final float top = bottom + (region.getTop() - region.getBottom())*vScale;
 
-                    vertices[verticesIndex++] = new TextVertex(new Vector2(left,  bottom),
-                            new Vector2(region.getLeft(), region.getBottom()), color);
-                    vertices[verticesIndex++] = new TextVertex(new Vector2(right, bottom),
-                            new Vector2(region.getRight(), region.getBottom()), color);
-                    vertices[verticesIndex++] = new TextVertex(new Vector2(left, top),
-                            new Vector2(region.getLeft(), region.getTop()), color);
-                    vertices[verticesIndex] = new TextVertex(new Vector2(right, top),
-                            new Vector2(region.getRight(), region.getTop()), color);
+                    final TextVertex v0 = this.buffer[charCount*VERTEX_PER_CHAR];
+                    final TextVertex v1 = this.buffer[charCount*VERTEX_PER_CHAR + 1];
+                    final TextVertex v2 = this.buffer[charCount*VERTEX_PER_CHAR + 2];
+                    final TextVertex v3 = this.buffer[charCount*VERTEX_PER_CHAR + 3];
+
+                    v0.position.setX(left);
+                    v0.position.setY(bottom);
+                    v0.coords.setX(region.getLeft());
+                    v0.coords.setY(region.getBottom());
+                    v0.color.set(color);
+
+                    v1.position.setX(right);
+                    v1.position.setY(bottom);
+                    v1.coords.setX(region.getRight());
+                    v1.coords.setY(region.getBottom());
+                    v1.color.set(color);
+
+                    v2.position.setX(left);
+                    v2.position.setY(top);
+                    v2.coords.setX(region.getLeft());
+                    v2.coords.setY(region.getTop());
+                    v2.color.set(color);
+
+                    v3.position.setX(right);
+                    v3.position.setY(top);
+                    v3.coords.setX(region.getRight());
+                    v3.coords.setY(region.getTop());
+                    v3.color.set(color);
 
                     caret.advance(glyph.getXAdvance()*hScale);
                     charCount++;
@@ -97,18 +152,14 @@ public class TextRenderer {
             }
         }
 
-        final IndexBuffer indexBuffer = this.generateIndexBuffer(charCount);
-
-        final VertexBuffer vertexBuffer = new VertexBuffer(TextVertex.VERTEX_DESCRIPTION, BufferUsage.STATIC_DRAW,
-                charCount*VERTEX_PER_CHAR);
-        vertexBuffer.setData(vertices, charCount*VERTEX_PER_CHAR);
+        this.vertexBuffer.setData(this.buffer, charCount*VERTEX_PER_CHAR);
 
         font.getBitmap().bind();
         this.shader.bind();
         this.shader.setUniform("thickness", 0.5f);
         this.shader.setUniform("smoothing", font.computeSmoothing(this.configuration.getYResolution()*scale));
-        vertexBuffer.bind();
-        indexBuffer.bind();
+        this.vertexBuffer.bind();
+        this.indexBuffer.bind();
 
         Engine.setBlendMode(BlendMode.TRANSPARENCY);
         GL11.glDrawElements(GL11.GL_TRIANGLES, charCount*INDEX_PER_CHAR, GL11.GL_UNSIGNED_INT, 0);
@@ -118,23 +169,6 @@ public class TextRenderer {
         IndexBuffer.unbind();
         VertexBuffer.unbind();
         Texture.unbind();
-
-        vertexBuffer.destroy();
-        indexBuffer.destroy();
-    }
-
-    private IndexBuffer generateIndexBuffer(int size) {
-        final int[] indices = new int[size*INDEX_PER_CHAR];
-        for(int i = 0 ; i < size; i++) {
-            int indicesIndex = i*INDEX_PER_CHAR;
-            indices[indicesIndex++] = i*VERTEX_PER_CHAR;
-            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 1;
-            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 2;
-            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 2;
-            indices[indicesIndex++] = i*VERTEX_PER_CHAR + 1;
-            indices[indicesIndex] = i*VERTEX_PER_CHAR + 3;
-        }
-        return new IndexBuffer(BufferUsage.STATIC_DRAW, indices);
     }
 
     /**
