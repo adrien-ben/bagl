5390892b95fcaa43b3e69c1966d4e1e308627946
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-05-18 16:15:13 +0200

    feat (model) : added support for multi-mesh models to obj parser

--- src/main/java/com/adrien/games/bagl/parser/model/ModelParser.java
+++ src/main/java/com/adrien/games/bagl/parser/model/ModelParser.java
@@ -1,14 +1,13 @@
 package com.adrien.games.bagl.parser.model;
 
-import com.adrien.games.bagl.rendering.Mesh;
 import com.adrien.games.bagl.rendering.Model;
 
 public interface ModelParser {
 
     /**
-     * Parses a file to create a {@link Mesh}.
+     * Parses a file to create a {@link Model}.
      * @param filePath The path to the file.
-     * @return A {@link Mesh}.
+     * @return A {@link Model}.
      */
     Model parse(String filePath);
 
--- src/main/java/com/adrien/games/bagl/parser/model/ObjParser.java
+++ src/main/java/com/adrien/games/bagl/parser/model/ObjParser.java
@@ -29,21 +29,20 @@ public class ObjParser implements ModelParser {
     private static final String TEXTURE_LINE_FLAG = "vt";
     private static final String NORMAL_LINE_FLAG = "vn";
     private static final String FACE_LINE_FLAG = "f";
+    private static final String OBJECT_LINE_FLAG = "o";
     private static final String MTL_LIB_FLAG = "mtllib";
     private static final String USE_MTL_FLAG = "usemtl";
 
+    private static final String DEFAULT_MESH_NAME = "default";
+
     private String currentFile;
     private int currentLine;
-    private final List<Vector3> positions = new ArrayList<>();
-    private final List<Vector2> coords = new ArrayList<>();
-    private final List<Vector3> normals = new ArrayList<>();
-    private final List<Vector3> tangents = new ArrayList<>();
-    private final List<Face> faces = new ArrayList<>();
-    private final List<Integer> faceIndices = new ArrayList<>();
-    private final Map<Face, Integer> faceToIndexMap = new HashMap<>();
+
+    private final Map<String, MeshBuilder> builders = new HashMap<>();
+    private MeshBuilder currentBuilder = new MeshBuilder(DEFAULT_MESH_NAME);
+
     private final MtlParser mtlParser = new MtlParser();
     private final Map<String, Material> materialLib = new HashMap<>();
-    private Material usedMaterial;
 
     /**
      * {@inheritDoc}
@@ -53,6 +52,7 @@ public class ObjParser implements ModelParser {
      * <li>texture coordinates (u, v)
      * <li>normals (x, y, z)
      * <li>triangle faces (each vertex MUST reference a position, coordinates and normal)
+     * <li>Multi-mesh models</li>
      */
     @Override
     public Model parse(String filePath) {
@@ -65,105 +65,25 @@ public class ObjParser implements ModelParser {
             log.error("Failed to parse file '{}'.", filePath, e);
             throw new RuntimeException("Failed to parse model file", e);
         }
-        this.computeTangents();
-        return model.addMesh(this.build());
-    }
-
-    private void computeTangents() {
-
-        Vector3[] tangents = new Vector3[this.faces.size()];
-
-        for(int i = 0; i < this.faceIndices.size(); i+=3) {
-
-            int index0 = this.faceIndices.get(i);
-            int index1 = this.faceIndices.get(i + 1);
-            int index2 = this.faceIndices.get(i + 2);
-
-            Face face0 = this.faces.get(index0);
-            Face face1 = this.faces.get(index1);
-            Face face2 = this.faces.get(index2);
-
-            Vector3 pos0 = this.positions.get(face0.getPositionIndex());
-            Vector3 pos1 = this.positions.get(face1.getPositionIndex());
-            Vector3 pos2 = this.positions.get(face2.getPositionIndex());
-            Vector3 edge1 = Vector3.sub(pos1, pos0);
-            Vector3 edge2 = Vector3.sub(pos2, pos0);
-
-            Vector2 coords0 = this.coords.get(face0.getCoordsIndex());
-            Vector2 coords1 = this.coords.get(face1.getCoordsIndex());
-            Vector2 coords2 = this.coords.get(face2.getCoordsIndex());
-            Vector2 deltaUVx = Vector2.sub(coords1, coords0);
-            Vector2 deltaUVy = Vector2.sub(coords2, coords0);
-
-            float f = 1/(deltaUVx.getX()*deltaUVy.getY() - deltaUVy.getX()*deltaUVx.getY());
-
-            float x = f*(deltaUVy.getY()*edge1.getX() - deltaUVx.getY()*edge2.getX());
-            float y = f*(deltaUVy.getY()*edge1.getY() - deltaUVx.getY()*edge2.getY());
-            float z = f*(deltaUVy.getY()*edge1.getZ() - deltaUVx.getY()*edge2.getZ());
-            Vector3 tangent = new Vector3(x, y, z);
-            tangent.normalise();
-
-            this.setTangentForFace(tangents, index0, tangent);
-            this.setTangentForFace(tangents, index1, tangent);
-            this.setTangentForFace(tangents, index2, tangent);
-        }
-
-        Collections.addAll(this.tangents, tangents);
-
-    }
-
-    private void setTangentForFace(Vector3[] tangents, int index, Vector3 tangent) {
-        Vector3 currentTangent = tangents[index];
-        if(Objects.isNull(currentTangent)) {
-            currentTangent = new Vector3(tangent);
-            tangents[index] = currentTangent;
-        } else {
-            currentTangent.average(tangent);
-        }
-    }
-
-    private Mesh build() {
-        int vertexCount = this.faces.size();
-        Vertex[] vertexArray = new MeshVertex[vertexCount];
-        for(int i = 0; i < vertexCount; i++) {
-            Face face = this.faces.get(i);
-            Vector3 position = this.positions.get(face.getPositionIndex());
-            Vector3 normal = this.normals.get(face.getNormalIndex());
-            Vector2 coord = this.coords.get(face.getCoordsIndex());
-            Vector3 tangent = this.tangents.get(i);
-            vertexArray[i] = new MeshVertex(position, normal, coord, tangent);
-        }
-
-        int indexCount = this.faceIndices.size();
-        int[] indexArray = new int[indexCount];
-        for(int i = 0; i < indexCount; i++) {
-            indexArray[i] = this.faceIndices.get(i);
-        }
-
-        VertexBuffer vertexBuffer = new VertexBuffer(MeshVertex.DESCRIPTION, BufferUsage.STATIC_DRAW, vertexArray);
-        IndexBuffer indexBuffer = new IndexBuffer(BufferUsage.STATIC_DRAW, indexArray);
-        return new Mesh(vertexBuffer, indexBuffer, this.usedMaterial);
+        this.builders.put(this.currentBuilder.meshName, this.currentBuilder);
+        this.builders.values().stream().filter(MeshBuilder::isNotEmpty).map(MeshBuilder::build).forEach(model::addMesh);
+        return model;
     }
 
     private void resetParser(String filePath) {
         this.currentFile = filePath;
         this.currentLine = 0;
-        this.positions.clear();
-        this.coords.clear();
-        this.normals.clear();
-        this.tangents.clear();
-        this.faces.clear();
-        this.faceIndices.clear();
-        this.faceToIndexMap.clear();
+        this.builders.clear();
+        MeshBuilder.clear();
+        this.currentBuilder = new MeshBuilder(DEFAULT_MESH_NAME);
         this.materialLib.clear();
-        this.usedMaterial = null;
     }
 
     private void parseLine(String line) {
         this.currentLine++;
-        String[] tokens = line.split(SPACE_SEP);
+        final String[] tokens = line.split(SPACE_SEP);
         if(tokens.length > 0) {
-            String first = tokens[0];
+            final String first = tokens[0];
             if(POSITION_LINE_FLAG.equals(first)) {
                 this.parsePosition(tokens);
             } else if(TEXTURE_LINE_FLAG.equals(first)) {
@@ -172,7 +92,9 @@ public class ObjParser implements ModelParser {
                 this.parseNormal(tokens);
             } else if(FACE_LINE_FLAG.equals(first)) {
                 this.parseFace(tokens);
-            } else if(MTL_LIB_FLAG.equals(first)) {
+            } else if(OBJECT_LINE_FLAG.equals(first)) {
+                this.parseNewObject(tokens);
+            }else if(MTL_LIB_FLAG.equals(first)) {
                 this.parseMtlLib(tokens);
             } else if(USE_MTL_FLAG.equals(first)) {
                 this.parseUseMtl(tokens);
@@ -184,10 +106,10 @@ public class ObjParser implements ModelParser {
 
     private void parsePosition(String[] tokens) {
         if(tokens.length >= 4) {
-            float x = Float.parseFloat(tokens[1]);
-            float y = Float.parseFloat(tokens[2]);
-            float z = Float.parseFloat(tokens[3]);
-            this.positions.add(new Vector3(x, y, z));
+            final float x = Float.parseFloat(tokens[1]);
+            final float y = Float.parseFloat(tokens[2]);
+            final  float z = Float.parseFloat(tokens[3]);
+            MeshBuilder.positions.add(new Vector3(x, y, z));
         } else {
             this.handleParseError("Found position with less than 3 components at line " +  this.currentLine + ".");
         }
@@ -195,9 +117,9 @@ public class ObjParser implements ModelParser {
 
     private void parseTextureCoords(String[] tokens) {
         if(tokens.length >= 3) {
-            float u = Float.parseFloat(tokens[1]);
-            float v = Float.parseFloat(tokens[2]);
-            this.coords.add(new Vector2(u, v));
+            final float u = Float.parseFloat(tokens[1]);
+            final float v = Float.parseFloat(tokens[2]);
+            MeshBuilder.coords.add(new Vector2(u, v));
         } else {
             this.handleParseError("Found texture coordinates with less than 2 components at line " + this.currentLine + ".");
         }
@@ -205,10 +127,10 @@ public class ObjParser implements ModelParser {
 
     private void parseNormal(String[] tokens) {
         if(tokens.length >= 4) {
-            float x = Float.parseFloat(tokens[1]);
-            float y = Float.parseFloat(tokens[2]);
-            float z = Float.parseFloat(tokens[3]);
-            this.normals.add(new Vector3(x, y, z));
+            final float x = Float.parseFloat(tokens[1]);
+            final float y = Float.parseFloat(tokens[2]);
+            final float z = Float.parseFloat(tokens[3]);
+            MeshBuilder.normals.add(new Vector3(x, y, z));
         } else {
             this.handleParseError("Found normal with less than 3 components at line " + this.currentLine + ".");
         }
@@ -226,31 +148,40 @@ public class ObjParser implements ModelParser {
     }
 
     private void parseFaceVertex(String faceVertex) {
-        String[] tokens = faceVertex.split("/");
+        final String[] tokens = faceVertex.split("/");
         if(tokens.length >= 3 && StringUtils.isNotBlank(tokens[0]) &&
                 StringUtils.isNotBlank(tokens[1]) && StringUtils.isNotBlank(tokens[2])) {
-            int positionIndex = Integer.parseInt(tokens[0]) - 1;
-            int textCoordIndex = Integer.parseInt(tokens[1]) - 1;
-            int normalIndex = Integer.parseInt(tokens[2]) - 1;
-            Face face = new Face(positionIndex, normalIndex, textCoordIndex);
-            Integer faceIndex = this.faceToIndexMap.get(face);
-            if(Objects.isNull(faceIndex)) {
-                faceIndex = this.faces.size();
-                this.faces.add(face);
-                this.faceToIndexMap.put(face, faceIndex);
-            }
-            this.faceIndices.add(faceIndex);
+            final int positionIndex = Integer.parseInt(tokens[0]) - 1;
+            final int textCoordIndex = Integer.parseInt(tokens[1]) - 1;
+            final int normalIndex = Integer.parseInt(tokens[2]) - 1;
+            final Face face = new Face(positionIndex, normalIndex, textCoordIndex);
+
+            final Integer faceIndex = this.currentBuilder.faceToIndexMap.computeIfAbsent(face, key -> {
+                final int nextFaceIndex = this.currentBuilder.faces.size();
+                this.currentBuilder.faces.add(key);
+                return nextFaceIndex;
+            });
+            this.currentBuilder.faceIndices.add(faceIndex);
         } else {
             this.handleParseError("A face vertex does not reference a position, coords and/or a normal at line " +
                     this.currentLine + ". Only triangle faces are supported.");
         }
     }
 
+    private void parseNewObject(String[] tokens) {
+        if(tokens.length > 1) {
+            this.builders.put(this.currentBuilder.meshName, this.currentBuilder);
+            this.currentBuilder = new MeshBuilder(tokens[1]);
+        } else {
+            this.handleParseError("A new object declaration misses an object name at line " + this.currentLine + ".");
+        }
+    }
+
     private void parseMtlLib(String[] tokens) {
         if(tokens.length > 1) {
-            String fileName = this.getContentInBrackets(tokens[1]);
-            String folderPath = Paths.get(this.currentFile).getParent().toString();
-            String mtlPath = folderPath + "/" + fileName;
+            final String fileName = this.getContentInBrackets(tokens[1]);
+            final String folderPath = Paths.get(this.currentFile).getParent().toString();
+            final String mtlPath = folderPath + "/" + fileName;
             this.mtlParser.parse(mtlPath).forEach(this.materialLib::put);
         } else {
             this.handleParseError("A material lib reference is incorrect at line " + this.currentLine + ".");
@@ -258,10 +189,10 @@ public class ObjParser implements ModelParser {
     }
 
     private void parseUseMtl(String[] tokens) {
-        String mtlName = getContentInBrackets(tokens[1]);
-        this.usedMaterial = materialLib.get(mtlName);
-        if(Objects.isNull(this.usedMaterial)) {
-            handleParseError("Material '" + mtlName + "' at line '" + this.currentLine + " does not exists.");
+        final String mtlName = getContentInBrackets(tokens[1]);
+        this.currentBuilder.material = materialLib.get(mtlName);
+        if(Objects.isNull(this.currentBuilder.material)) {
+            this.handleParseError("Material '" + mtlName + "' at line '" + this.currentLine + " does not exists.");
         }
     }
 
@@ -272,6 +203,115 @@ public class ObjParser implements ModelParser {
     private void handleParseError(String error) {
         log.error(error);
         throw new RuntimeException(error);
+    }
+
+    /**
+     * Internal mesh builder.
+     */
+    private static class MeshBuilder {
+
+        final static List<Vector3> positions = new ArrayList<>();
+        final static List<Vector2> coords = new ArrayList<>();
+        final static List<Vector3> normals = new ArrayList<>();
+
+        final String meshName;
+        final List<Vector3> tangents = new ArrayList<>();
+        final List<Face> faces = new ArrayList<>();
+        final List<Integer> faceIndices = new ArrayList<>();
+        final Map<Face, Integer> faceToIndexMap = new HashMap<>();
+        Material material;
+
+        MeshBuilder(String meshName) {
+            this.meshName = meshName;
+        }
+
+        boolean isNotEmpty() {
+            return !this.faces.isEmpty();
+        }
+
+        Mesh build() {
+            this.computeTangents();
+            final int vertexCount = this.faces.size();
+            final Vertex[] vertexArray = new MeshVertex[vertexCount];
+            for(int i = 0; i < vertexCount; i++) {
+                final Face face = this.faces.get(i);
+                final Vector3 position = positions.get(face.getPositionIndex());
+                final Vector3 normal = normals.get(face.getNormalIndex());
+                final Vector2 coord = coords.get(face.getCoordsIndex());
+                final Vector3 tangent = this.tangents.get(i);
+                vertexArray[i] = new MeshVertex(position, normal, coord, tangent);
+            }
+
+            final int indexCount = this.faceIndices.size();
+            final int[] indexArray = new int[indexCount];
+            for(int i = 0; i < indexCount; i++) {
+                indexArray[i] = this.faceIndices.get(i);
+            }
+
+            final VertexBuffer vertexBuffer = new VertexBuffer(MeshVertex.DESCRIPTION, BufferUsage.STATIC_DRAW, vertexArray);
+            final IndexBuffer indexBuffer = new IndexBuffer(BufferUsage.STATIC_DRAW, indexArray);
+            return new Mesh(vertexBuffer, indexBuffer, this.material);
+        }
+
+        void computeTangents() {
+
+            final Vector3[] tangents = new Vector3[this.faces.size()];
+
+            for(int i = 0; i < this.faceIndices.size(); i+=3) {
+
+                final int index0 = this.faceIndices.get(i);
+                final int index1 = this.faceIndices.get(i + 1);
+                final int index2 = this.faceIndices.get(i + 2);
+
+                final Face face0 = this.faces.get(index0);
+                final Face face1 = this.faces.get(index1);
+                final Face face2 = this.faces.get(index2);
+
+                final Vector3 pos0 = positions.get(face0.getPositionIndex());
+                final Vector3 pos1 = positions.get(face1.getPositionIndex());
+                final Vector3 pos2 = positions.get(face2.getPositionIndex());
+                final Vector3 edge1 = Vector3.sub(pos1, pos0);
+                final Vector3 edge2 = Vector3.sub(pos2, pos0);
+
+                final Vector2 coords0 = coords.get(face0.getCoordsIndex());
+                final Vector2 coords1 = coords.get(face1.getCoordsIndex());
+                final Vector2 coords2 = coords.get(face2.getCoordsIndex());
+                final Vector2 deltaUVx = Vector2.sub(coords1, coords0);
+                final Vector2 deltaUVy = Vector2.sub(coords2, coords0);
+
+                final float f = 1/(deltaUVx.getX()*deltaUVy.getY() - deltaUVy.getX()*deltaUVx.getY());
+
+                final float x = f*(deltaUVy.getY()*edge1.getX() - deltaUVx.getY()*edge2.getX());
+                final float y = f*(deltaUVy.getY()*edge1.getY() - deltaUVx.getY()*edge2.getY());
+                final float z = f*(deltaUVy.getY()*edge1.getZ() - deltaUVx.getY()*edge2.getZ());
+                final Vector3 tangent = new Vector3(x, y, z);
+                tangent.normalise();
+
+                this.setTangentForFace(tangents, index0, tangent);
+                this.setTangentForFace(tangents, index1, tangent);
+                this.setTangentForFace(tangents, index2, tangent);
+            }
+
+            Collections.addAll(this.tangents, tangents);
+
+        }
+
+        void setTangentForFace(Vector3[] tangents, int index, Vector3 tangent) {
+            Vector3 currentTangent = tangents[index];
+            if(Objects.isNull(currentTangent)) {
+                currentTangent = new Vector3(tangent);
+                tangents[index] = currentTangent;
+            } else {
+                currentTangent.average(tangent);
+            }
+        }
+
+        static void clear() {
+            positions.clear();
+            coords.clear();
+            normals.clear();
+        }
+
     }
 
 }
