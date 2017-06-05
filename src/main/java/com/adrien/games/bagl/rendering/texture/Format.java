c9908db9d429b9f56b11770f388da9f997352029
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2017-06-05 09:30:54 +0200

    feat (texture) : added RGBA16 texture format

--- src/main/java/com/adrien/games/bagl/rendering/texture/Format.java
+++ src/main/java/com/adrien/games/bagl/rendering/texture/Format.java
@@ -7,6 +7,7 @@ import static org.lwjgl.opengl.GL30.GL_R8;
 public enum Format {
 
     RGBA8(GL_RGBA8, GL_RGBA),
+    RGBA16(GL_RGBA16, GL_RGBA),
     RGB8(GL_RGB8, GL_RGB),
     RED8(GL_R8, GL_RED),
     ALPHA8(GL_ALPHA8, GL_ALPHA),
