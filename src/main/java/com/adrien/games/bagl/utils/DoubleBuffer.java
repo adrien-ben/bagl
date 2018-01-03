76de6981193a5e341d7a45a0bfd99c0bc51778a5
Adrien Bennadji <adrien.bennadji@live.fr>
2018-01-03 20:57:11 +0100

    feat (*) : Added double buffer class

---
+++ src/main/java/com/adrien/games/bagl/utils/DoubleBuffer.java
@@ -0,0 +1,57 @@
+package com.adrien.games.bagl.utils;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Double buffer
+ *
+ * @author adrien
+ */
+public class DoubleBuffer<T> {
+
+    private T readBuffer;
+
+    private T writeBuffer;
+
+    /**
+     * Constructs the double buffer by initializing the read and write buffer
+     * by calling a supplier function
+     *
+     * @param supplier The initializer function for the buffers
+     */
+    public DoubleBuffer(final Supplier<T> supplier) {
+        this.readBuffer = supplier.get();
+        this.writeBuffer = supplier.get();
+    }
+
+    /**
+     * Swaps the read and write buffers
+     */
+    public void swap() {
+        final T tmp = this.readBuffer;
+        this.readBuffer = this.writeBuffer;
+        this.writeBuffer = tmp;
+    }
+
+    /**
+     * Applies a consumer to both buffers
+     * <p>
+     * Can be useful to reset the buffer for example
+     *
+     * @param consumer The consumer to apply on both buffers
+     */
+    public void apply(final Consumer<T> consumer) {
+        consumer.accept(this.readBuffer);
+        consumer.accept(this.writeBuffer);
+    }
+
+    public T getReadBuffer() {
+        return readBuffer;
+    }
+
+    public T getWriteBuffer() {
+        return writeBuffer;
+    }
+
+}
