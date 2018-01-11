080939c9de011727d06d08ff1f138e7dace50e8d
Adrien Bennadji <adrien.bennadji@live.fr>
2018-01-11 21:31:48 +0100

    feat (*) : Remove Vertex/IndexBuffer from ParticleRenderer class

--- src/main/java/com/adrien/games/bagl/rendering/particles/ParticleRenderer.java
+++ src/main/java/com/adrien/games/bagl/rendering/particles/ParticleRenderer.java
@@ -1,35 +1,56 @@
 package com.adrien.games.bagl.rendering.particles;
 
 import com.adrien.games.bagl.core.Camera;
-import com.adrien.games.bagl.core.Color;
 import com.adrien.games.bagl.core.Engine;
 import com.adrien.games.bagl.core.Time;
 import com.adrien.games.bagl.core.math.Vector3;
 import com.adrien.games.bagl.rendering.BlendMode;
-import com.adrien.games.bagl.rendering.BufferUsage;
 import com.adrien.games.bagl.rendering.Shader;
-import com.adrien.games.bagl.rendering.VertexBuffer;
 import com.adrien.games.bagl.rendering.texture.Texture;
-import com.adrien.games.bagl.rendering.vertex.Vertex;
-import com.adrien.games.bagl.rendering.vertex.VertexDescription;
-import com.adrien.games.bagl.rendering.vertex.VertexElement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+import org.lwjgl.system.MemoryUtil;
 
+import java.nio.FloatBuffer;
 import java.util.*;
 
 /**
- * Renders particles using OpenGL geometry shaders.
+ * Render particles using OpenGL geometry shaders
+ *
+ * @author adrien
  */
 public class ParticleRenderer {
 
     private static final ParticleComparator COMPARATOR = new ParticleComparator();
+    private static final int FLOAT_SIZE_IN_BYTES = Float.SIZE / 8;
+    private static final int ELEMENTS_PER_VERTEX = 8;
+    private static final int VERTEX_STRIDE = ELEMENTS_PER_VERTEX * FLOAT_SIZE_IN_BYTES;
+    private static final int POSITION_INDEX = 0;
+    private static final int POSITION_OFFSET = 0;
+    private static final int ELEMENTS_PER_POSITION = 3;
+    private static final int COLOR_INDEX = 1;
+    private static final int COLOR_OFFSET = 3 * FLOAT_SIZE_IN_BYTES;
+    private static final int ELEMENTS_PER_COLOR = 4;
+    private static final int SIZE_INDEX = 2;
+    private static final int SIZE_OFFSET = 7 * FLOAT_SIZE_IN_BYTES;
+    private static final int ELEMENTS_PER_SIZE = 1;
+
+    private static Logger log = LogManager.getLogger(ParticleRenderer.class);
 
     private final Shader shader;
-    private final VertexBuffer vBuffer;
-    private final ParticleVertex[] vertices;
+    private final FloatBuffer vertices;
+    private final int vaoId;
+    private final int vboId;
     private final List<Particle> particlesToRender;
     private final Time timer;
 
+    /**
+     * Construct the particle renderer
+     */
     public ParticleRenderer() {
         this.shader = new Shader()
                 .addVertexShader("/particles/particles.vert")
@@ -37,21 +58,45 @@ public class ParticleRenderer {
                 .addGeometryShader("/particles/particles.geom")
                 .compile();
 
-        this.vertices = new ParticleVertex[ParticleEmitter.MAX_PARTICLE_COUNT];
-        for (int i = 0; i < ParticleEmitter.MAX_PARTICLE_COUNT; i++) {
-            this.vertices[i] = new ParticleVertex(new Vector3(), Color.WHITE, 1f);
-        }
-        this.vBuffer = new VertexBuffer(ParticleVertex.getDescription(), BufferUsage.STREAM_DRAW, this.vertices);
+        this.vertices = MemoryUtil.memAllocFloat(ParticleEmitter.MAX_PARTICLE_COUNT * ELEMENTS_PER_VERTEX);
+        this.vaoId = GL30.glGenVertexArrays();
+        this.vboId = GL15.glGenBuffers();
+        GL30.glBindVertexArray(this.vaoId);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboId);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertices, GL15.GL_STREAM_DRAW);
+
+        GL20.glEnableVertexAttribArray(POSITION_INDEX);
+        GL20.glVertexAttribPointer(POSITION_INDEX, ELEMENTS_PER_POSITION, GL11.GL_FLOAT, false, VERTEX_STRIDE, POSITION_OFFSET);
+
+        GL20.glEnableVertexAttribArray(COLOR_INDEX);
+        GL20.glVertexAttribPointer(COLOR_INDEX, ELEMENTS_PER_COLOR, GL11.GL_FLOAT, false, VERTEX_STRIDE, COLOR_OFFSET);
+
+        GL20.glEnableVertexAttribArray(SIZE_INDEX);
+        GL20.glVertexAttribPointer(SIZE_INDEX, ELEMENTS_PER_SIZE, GL11.GL_FLOAT, false, VERTEX_STRIDE, SIZE_OFFSET);
+
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        GL30.glBindVertexArray(0);
+
         this.particlesToRender = new ArrayList<>();
         this.timer = new Time();
     }
 
     /**
-     * Renders all particles owned by the passed in {@link ParticleEmitter} from
-     * a {@link Camera} point of view.
+     * Release resources
+     */
+    public void destroy() {
+        this.shader.destroy();
+        MemoryUtil.memFree(this.vertices);
+        GL15.glDeleteBuffers(this.vboId);
+        GL30.glDeleteVertexArrays(this.vaoId);
+    }
+
+    /**
+     * Render all particles owned by the passed in {@link ParticleEmitter} from
+     * a {@link Camera} point of view
      *
-     * @param emitter The emitter to render.
-     * @param camera  The camera.
+     * @param emitter The emitter to render
+     * @param camera  The camera
      */
     public void render(final ParticleEmitter emitter, final Camera camera) {
         int particleToRender = 0;
@@ -59,24 +104,29 @@ public class ParticleRenderer {
         this.timer.update();
         this.particlesToRender.clear();
         Arrays.stream(emitter.getParticles()).filter(Particle::isAlive).forEach(this.particlesToRender::add);
-        System.out.println("Retrieving active particles: " + this.timer.getElapsedTime());
+        log.debug("Retrieving active particles: {}", this.timer.getElapsedTime());
 
         if (emitter.getBlendMode() != BlendMode.ADDITIVE) {
             this.timer.update();
             COMPARATOR.setCamera(camera);
             this.particlesToRender.sort(COMPARATOR);
-            System.out.println("Sorting particles: " + this.timer.getElapsedTime());
+            log.debug("Sorting particles: {}", this.timer.getElapsedTime());
         }
 
         this.timer.update();
-        for (Particle p : this.particlesToRender) {
-            final ParticleVertex particleVertex = this.vertices[particleToRender];
-            particleVertex.position = p.getPosition();
-            particleVertex.color = p.getColor();
-            particleVertex.size = p.getSize();
+        for (final Particle p : this.particlesToRender) {
+            final int index = particleToRender * ELEMENTS_PER_VERTEX;
+            this.vertices.put(index, p.getPosition().getX());
+            this.vertices.put(index + 1, p.getPosition().getY());
+            this.vertices.put(index + 2, p.getPosition().getZ());
+            this.vertices.put(index + 3, p.getColor().getRed());
+            this.vertices.put(index + 4, p.getColor().getGreen());
+            this.vertices.put(index + 5, p.getColor().getBlue());
+            this.vertices.put(index + 6, p.getColor().getAlpha());
+            this.vertices.put(index + 7, p.getSize());
             particleToRender++;
         }
-        System.out.println("Copying data to cpu buffer : " + this.timer.getElapsedTime());
+        log.debug("Copying data to cpu buffer : {}", this.timer.getElapsedTime());
 
         boolean hasTexture;
         final Optional<Texture> texture = emitter.getTexture();
@@ -88,70 +138,32 @@ public class ParticleRenderer {
         this.shader.setUniform("hasTexture", hasTexture);
         this.shader.setUniform("camera.view", camera.getView());
         this.shader.setUniform("camera.viewProj", camera.getViewProj());
-        this.vBuffer.bind();
+        GL30.glBindVertexArray(this.vaoId);
 
         this.timer.update();
-        this.vBuffer.setData(this.vertices, particleToRender);
-        System.out.println("Copying data to gpu : " + this.timer.getElapsedTime());
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboId);
+        GL15.glBufferSubData(GL15.GL_ARRAY_BUFFER, 0, this.vertices);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        log.debug("Copying data to gpu : {}", this.timer.getElapsedTime());
 
         Engine.setBlendMode(emitter.getBlendMode());
         GL11.glDepthMask(false);
 
         this.timer.update();
         GL11.glDrawArrays(GL11.GL_POINTS, 0, particleToRender);
-        System.out.println("Rendering " + particleToRender + " particles : " + this.timer.getElapsedTime());
+        log.debug("Rendering {} particles : {}", particleToRender, this.timer.getElapsedTime());
 
         GL11.glDepthMask(true);
         Engine.setBlendMode(BlendMode.NONE);
 
-        VertexBuffer.unbind();
+        GL30.glBindVertexArray(0);
         Shader.unbind();
         Texture.unbind();
     }
 
     /**
-     * Release resources
-     */
-    public void destroy() {
-        this.shader.destroy();
-        this.vBuffer.destroy();
-    }
-
-    /**
-     * Particle vertex class.
-     */
-    private static class ParticleVertex implements Vertex {
-
-        private Vector3 position;
-        private Color color;
-        private float size;
-
-        ParticleVertex(final Vector3 position, final Color color, final float size) {
-            this.position = position;
-            this.color = color;
-            this.size = size;
-        }
-
-        @Override
-        public float[] getData() {
-            return new float[]{this.position.getX(), this.position.getY(), this.position.getZ(), this.color.getRed(),
-                    this.color.getGreen(), this.color.getBlue(), this.color.getAlpha(), this.size};
-        }
-
-        public static VertexDescription getDescription() {
-            return new VertexDescription(new VertexElement[]
-                    {
-                            new VertexElement(0, 3, 0),
-                            new VertexElement(1, 4, 3),
-                            new VertexElement(2, 1, 7)
-                    });
-        }
-
-    }
-
-    /**
      * This comparators is used to sort particles from the furthest aways from
-     * the camera to the closest.
+     * the camera to the closest
      */
     private static class ParticleComparator implements Comparator<Particle> {
 
@@ -171,7 +183,5 @@ public class ParticleRenderer {
         public void setCamera(final Camera camera) {
             this.camera = camera;
         }
-
     }
-
 }
