576a30eac1c41f0cabc546d384d393fe7fcb0b6b
Adrien Bennadji <adrien.bennadji@live.fr>
2018-01-12 06:52:26 +0100

    feat (*) : Add new VertexBuffer class

---
+++ src/main/java/com/adrien/games/bagl/rendering/vertex/VertexBufferParams.java
@@ -0,0 +1,89 @@
+package com.adrien.games.bagl.rendering.vertex;
+
+import com.adrien.games.bagl.rendering.BufferUsage;
+import com.adrien.games.bagl.rendering.DataType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Vertex buffer parameters
+ * <p>
+ * This class contains parameters needed to build a
+ * vertex buffer
+ * <p>
+ * The available parameters are :
+ * <ul>
+ * <li>interlaced (boolean) : default = true
+ * <li>dataType ({@link DataType} : default = {@link DataType#FLOAT}
+ * <li>usage {@link BufferUsage} : default {@link BufferUsage#STATIC_DRAW}
+ * <li>elements (list of {@link VertexElement} : empty
+ * </ul>
+ * <p>
+ * The interlaced flag relates to the layout of vertex element in gpu memory.
+ * <br>For example lets take a buffer with positions and normals data with both
+ * position and normal containing a x and a y float values. If the
+ * flag is set to true then elements will be laid out as follows :
+ * <br> p1x|p1y|n1x|n1y|p2x|p2y|...|pNx|pNy|nNx|nNy
+ * <br>
+ * <br> But if it is set to false element will be stored as follows :
+ * <br> p1x|p1y|p2x|p2y|...pNx|pNy|n1x|n1y|n2x|n2y|...nNx|nNy
+ * <p>
+ * <br>
+ * Use example :
+ * <pre>
+ * <code>
+ * final VertexBufferParams params = new VertexBufferParams()
+ *     .interlaced(true)
+ *     .dataType(DataType.FLOAT)
+ *     .usage(BufferUsage.STATIC_DRAW)
+ *     .element(new VertexElement(0, 2, false))
+ *     .element(new VertexElement(1, 2, true));
+ * </code>
+ * </pre>
+ *
+ * @author EX477
+ */
+public class VertexBufferParams {
+
+    private boolean interlaced = true;
+    private DataType dataType = DataType.FLOAT;
+    private BufferUsage usage = BufferUsage.STATIC_DRAW;
+    private List<VertexElement> elements = new ArrayList<>();
+
+    public VertexBufferParams interlaced(final boolean interlaced) {
+        this.interlaced = interlaced;
+        return this;
+    }
+
+    public boolean isInterlaced() {
+        return this.interlaced;
+    }
+
+    public VertexBufferParams dataType(final DataType dataType) {
+        this.dataType = dataType;
+        return this;
+    }
+
+    public DataType getDataType() {
+        return this.dataType;
+    }
+
+    public VertexBufferParams usage(final BufferUsage usage) {
+        this.usage = usage;
+        return this;
+    }
+
+    public BufferUsage getUsage() {
+        return this.usage;
+    }
+
+    public VertexBufferParams element(final VertexElement element) {
+        this.elements.add(element);
+        return this;
+    }
+
+    public List<VertexElement> getElements() {
+        return this.elements;
+    }
+}
