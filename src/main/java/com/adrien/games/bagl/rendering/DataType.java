ec46b6417f4c6dbc91235265c0ae8fb63e3ac8e3
Adrien Bennadji <adrien.bennadji@live.fr>
2018-01-12 22:09:03 +0100

    feat (*) : Add unsigned int/short/byte to DataType

--- src/main/java/com/adrien/games/bagl/rendering/DataType.java
+++ src/main/java/com/adrien/games/bagl/rendering/DataType.java
@@ -12,8 +12,11 @@ public enum DataType {
     DOUBLE(GL11.GL_DOUBLE, Double.SIZE / 8),
     FLOAT(GL11.GL_FLOAT, Float.SIZE / 8),
     INT(GL11.GL_INT, Integer.SIZE / 8),
+    UNSIGNED_INT(GL11.GL_UNSIGNED_INT, Integer.SIZE / 8),
     SHORT(GL11.GL_SHORT, Short.SIZE / 8),
-    BYTE(GL11.GL_BYTE, Byte.SIZE / 8);
+    UNSIGNED_SHORT(GL11.GL_UNSIGNED_SHORT, Short.SIZE / 8),
+    BYTE(GL11.GL_BYTE, Byte.SIZE / 8),
+    UNSIGNED_BYTE(GL11.GL_UNSIGNED_BYTE, Byte.SIZE / 8);
 
     private final int glCode;
     private final int size;
