77d3c1a03a9cfbbd56f98d2aa54cf2dd274748c3
Adrien Bennadji <adrien.bennadji@live.fr>
2018-01-17 20:07:32 +0100

    feat (*) : Add a utility method to check file existence

--- src/main/java/com/adrien/games/bagl/utils/FileUtils.java
+++ src/main/java/com/adrien/games/bagl/utils/FileUtils.java
@@ -8,18 +8,26 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 
+/**
+ * File utility methods
+ *
+ * @author adrien
+ */
 public final class FileUtils {
 
+    /**
+     * Private constructor to prevent instantiation
+     */
     private FileUtils() {
     }
 
     /**
-     * Load the content of a file into a byte buffer.
+     * Load the content of a file into a byte buffer
      *
-     * @param filePath The path of the file to load.
-     * @return A {@link ByteBuffer} filled with the file content.
+     * @param filePath The path of the file to load
+     * @return A {@link ByteBuffer} filled with the file content
      */
-    public static ByteBuffer loadAsByteBuffer(String filePath) {
+    public static ByteBuffer loadAsByteBuffer(final String filePath) {
         try (final FileChannel channel = FileChannel.open(Paths.get(filePath))) {
             return channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
         } catch (IOException e) {
@@ -28,13 +36,28 @@ public final class FileUtils {
     }
 
     /**
-     * Gets the absolute path of a resource.
+     * Get the absolute path of a resource
      *
-     * @param resource The name of the resource.
-     * @return The absolute path of the resource as a {@link String}.
+     * @param resource The name of the resource
+     * @return The absolute path of the resource as a {@link String}
      */
-    public static String getResourceAbsolutePath(String resource) {
+    public static String getResourceAbsolutePath(final String resource) {
         return new File(FileUtils.class.getResource(resource).getFile()).getAbsolutePath();
+    }
+
+    /**
+     * Checks if a file exists
+     *
+     * @param filePath The path of the file
+     * @return The {@link File} if found
+     * @throws EngineException if the file does not exists
+     */
+    public static File checkFileExistence(final String filePath) {
+        final File file = new File(filePath);
+        if (!file.exists()) {
+            throw new EngineException("File not found: " + filePath);
+        }
+        return file;
     }
 
 }
