cf5a30725248271667af546566089a55c9313eca
Adrien Bennadji <adrien.bennadji@live.fr>
2018-04-17 21:33:08 +0200

    feat (java9) : add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module com.adrien.games.bagl {
+	requires java.base;
+	requires org.joml;
+	requires org.apache.logging.log4j;
+	requires org.lwjgl;
+	requires org.lwjgl.natives;
+	requires org.lwjgl.glfw;
+	requires org.lwjgl.glfw.natives;
+	requires org.lwjgl.jemalloc;
+	requires org.lwjgl.jemalloc.natives;
+	requires org.lwjgl.opengl;
+	requires org.lwjgl.opengl.natives;
+	requires org.lwjgl.stb;
+	requires org.lwjgl.stb.natives;
+	requires gltf.loader;
+
+	exports com.adrien.games.bagl.core;
+	exports com.adrien.games.bagl.rendering;
+	exports com.adrien.games.bagl.resource;
+	exports com.adrien.games.bagl.scene;
+	exports com.adrien.games.bagl.utils;
+}
