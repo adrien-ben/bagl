e8c89c43939a7b938a093fbd061b1ad28b21637d
Adrien Bennadji <adrien.bennadji@live.fr>
2018-08-15 23:13:12 +0200

    feat(deferred): Stabilize shadow
    Compute an offset matrix for the CSM projections to align them with world space texels

--- bagl-deferred/src/main/java/com/adrienben/games/bagl/deferred/shadow/CSMSplitsComputer.java
+++ bagl-deferred/src/main/java/com/adrienben/games/bagl/deferred/shadow/CSMSplitsComputer.java
@@ -1,5 +1,6 @@
 package com.adrienben.games.bagl.deferred.shadow;
 
+import com.adrienben.games.bagl.core.math.Vectors;
 import com.adrienben.games.bagl.core.utils.CollectionUtils;
 import com.adrienben.games.bagl.deferred.data.SceneRenderData;
 import org.joml.Matrix4f;
@@ -22,8 +23,11 @@ public class CSMSplitsComputer {
     private final CSMSplitValuesComputer csmSplitValuesComputer = new CSMSplitValuesComputer();
     private final List<CSMSplit> splits = CollectionUtils.createListWithDefaultValues(ArrayList::new, CascadedShadowMap.CASCADE_COUNT, CSMSplit::new);
 
-    private final Matrix4f lightWorld = new Matrix4f();
     private SceneRenderData sceneRenderData;
+
+    // Buffer used for computation for saving instantiation
+    private final Matrix4f matrixBuffer = new Matrix4f();
+    private final Vector3f vector3Buffer = new Vector3f();
 
     /**
      * Generates the cascaded shadow maps view-projection matrices.
@@ -48,7 +52,6 @@ public class CSMSplitsComputer {
     private void updateCSMSplits(final CSMSplit split) {
         computeSubFrustum(split);
         computeWorldSpaceBoundingSphere(split);
-        computeLightView(split);
         computeViewProjection(split);
     }
 
@@ -64,23 +67,38 @@ public class CSMSplitsComputer {
         subFrustum.computeBoundingSphere(boundingSphere);
     }
 
-    private void computeLightView(final CSMSplit split) {
-        final var light = sceneRenderData.getDirectionalLights().get(0);
-        final var boundingSphere = split.getBoundingSphere();
-        final var lightPosition = new Vector3f(light.getDirection()).normalize().mul(-boundingSphere.r).add(boundingSphere.x, boundingSphere.y, boundingSphere.z);
-        lightWorld.setLookAt(lightPosition.x, lightPosition.y, lightPosition.z, boundingSphere.x, boundingSphere.y, boundingSphere.z, 0f, 1f, 0f);
-    }
-
     private void computeViewProjection(final CSMSplit split) {
         final var viewProjection = split.getLightsViewProjection();
         final var boundingSphere = split.getBoundingSphere();
-        computeProjection(viewProjection, boundingSphere).mulOrthoAffine(lightWorld);
+        final var viewMatrix = computeLightView(split, matrixBuffer);
+        computeProjection(viewProjection, boundingSphere).mulOrthoAffine(viewMatrix);
+        alignViewProjectionWithTexels(viewProjection);
+    }
+
+    private Matrix4f computeLightView(final CSMSplit split, final Matrix4f destination) {
+        final var light = sceneRenderData.getDirectionalLights().get(0);
+        final var boundingSphere = split.getBoundingSphere();
+        final var lightPosition = vector3Buffer.set(light.getDirection()).normalize().mul(-boundingSphere.r).add(boundingSphere.x, boundingSphere.y, boundingSphere.z);
+        return destination.setLookAt(lightPosition.x, lightPosition.y, lightPosition.z, boundingSphere.x, boundingSphere.y, boundingSphere.z, 0f, 1f, 0f);
     }
 
     private Matrix4f computeProjection(final Matrix4f destination, final Spheref boundingSphere) {
         final float radius = boundingSphere.r;
         final float diameter = 2 * radius;
-        return destination.setOrtho(-radius, radius, -radius, radius, 0f, diameter);
+        return destination.setOrtho(-radius, radius, -radius, radius, 1f, diameter);
+    }
+
+    private void alignViewProjectionWithTexels(final Matrix4f viewProjection) {
+        final var texelOffset = computeTexelOffset(viewProjection, vector3Buffer);
+        final var fixMatrix = matrixBuffer.translation(texelOffset.x(), texelOffset.y(), texelOffset.z());
+        fixMatrix.mulAffine(viewProjection, viewProjection);
+    }
+
+    private Vector3f computeTexelOffset(final Matrix4f viewProjection, final Vector3f destination) {
+        final float halfCascadeResolution = CascadedShadowMap.CASCADE_RESOLUTION * 0.5f;
+        final var shadowOrigin = viewProjection.transformPosition(Vectors.VEC3_ZERO, destination).mul(halfCascadeResolution);
+        return destination.set((float) Math.round(shadowOrigin.x()) - shadowOrigin.x(), (float) Math.round(shadowOrigin.y()) - shadowOrigin.y(), 0f)
+                .div(halfCascadeResolution);
     }
 
     public CSMSplit getSplit(final int splitIndex) {
